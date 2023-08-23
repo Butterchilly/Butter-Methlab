1e22afed1a094803079ef700af3c913ef887e788
Ultra Scripts <88847062+Butterchilly@users.noreply.github.com>
2023-08-23 17:03:01 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,18 @@
 Config = {}
-Config.Debug = true -- If True You Can See The PolyZone
+Config.Debug = false -- If True You Can See The PolyZone
 
-Config.Use_Methlab_key = true -- If True Player Will Need (meth_key) To Enter The Lab
+Config.Use_Methlab_key = false -- If True Player Will Need (meth_key) To Enter The Lab
 
 Config.Blast = true -- If True The Player Will Be blasted If He Fail The Mini Game
 
+
+Config.ps_ui = {
+    NumberOfCircles = 4,
+    MS = 20,
+}
 Config.Resource_Names = {
     Core = 'qb-core',
     Target = 'qb-target',
-    Menu = 'qb-menu'
+    Menu = 'qb-menu',
+    MiniGame = 'ps-ui'
 }
